647b470746b9c1993ea371e27e5a13bea9732fb1
bovandersteene <bo.vandersteene@gmail.com>
2019-07-01 13:20:22 +0200

    initial commit

--- dist/public_api.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var tslib_1 = require("tslib");
-tslib_1.__exportStar(require("./destroy"), exports);
-tslib_1.__exportStar(require("./destroy.class"), exports);
